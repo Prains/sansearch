cad8fbfe2700b5963da87bcfe4037d4ac9487982
Prains <sleskov2013@yandex.ru>
2023-05-11 16:42:07 +0300

    123

--- components/Mainpage/Valueables/VBlock/VBlock.jsx
+++ components/Mainpage/Valueables/VBlock/VBlock.jsx
@@ -1,16 +1,13 @@
 import Subtitle from "@/components/ui/Subtitle";
 import Image from "next/image";
-import { comfort } from "@/images/Mainpage/Valueables";
 
-const VBlock = () => {
+const VBlock = ({ image, title, subtitle }) => {
   return (
-    <article className="flex items-center justify-center flex-col">
-      <Image src={comfort} />
-      <Subtitle type="secondary" className="mb-2">
-        Удобство
-      </Subtitle>
-      <p className="w-[160px] text-center leading-[15px] text-[12px]">
-        Поиск санкционных документов РФ, США, ЕС на одном ресурсе
+    <article className="flex items-center justify-center flex-col gap-2 p-10 bg-white-grey rounded-2xl">
+      <Image src={image} alt={title} />
+      <Subtitle type="secondary">{title}</Subtitle>
+      <p className="w-[160px] text-center leading-[15px] text-[12px] lg:text-[14px]">
+        {subtitle}
       </p>
     </article>
   );
