00109e4543264557d9ec7291f03c0577df157e99
Prains <sleskov2013@yandex.ru>
2023-05-11 16:16:47 +0300

    change globals css position

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import Header from "@/components/Header/Header";
-import "./globals.css";
 import Footer from "@/components/Footer/Footer";
+import "./globals.css";
 
 export const metadata = {
   title: "SanSearch",
