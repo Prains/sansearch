12536cc08aad57bc48a7338e8b98ad7a17ca9245
Prains <sleskov2013@yandex.ru>
2023-05-12 23:05:49 +0300

    fix input padding

--- components/MainPage/Main/MainInput/MainInput.jsx
+++ components/MainPage/Main/MainInput/MainInput.jsx
@@ -14,7 +14,7 @@ const MainInput = ({
       required={required}
       maxLength={254}
       onChange={onChange}
-      className="w-[280px] placeholder:pl-4 py-2 border-darker-orange border rounded-sm rounded-ee-none rounded-es-none lg:w-full lg:py-0 lg:h-[60px] lg:rounded-e-none lg:rounded-es-sm"
+      className="w-[280px] pl-4 py-2 border-darker-orange border rounded-sm rounded-ee-none rounded-es-none lg:w-full lg:py-0 lg:h-[60px] lg:rounded-e-none lg:rounded-es-sm"
     />
   );
 };
