b47f3eda506b09a64c29fc40e16ad76aac479dca
Prains <sleskov2013@yandex.ru>
2023-05-12 19:13:22 +0300

    fix vblock height

--- components/MainPage/Valueables/VBlock/VBlock.jsx
+++ components/MainPage/Valueables/VBlock/VBlock.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const VBlock = ({ image, title, subtitle }) => {
   return (
-    <article className="flex items-center justify-center flex-col gap-2 p-10 bg-white-grey rounded-2xl h-[270px]">
+    <article className="flex items-center justify-center flex-col gap-2 p-10 bg-white-grey rounded-2xl h-[240px] lg:h-[270px]">
       <Image src={image} alt={title} />
       <Subtitle type="secondary">{title}</Subtitle>
       <p className="w-[188px] text-center leading-[15px] text-[12px] lg:text-[14px] lg:leading-[17px]">
