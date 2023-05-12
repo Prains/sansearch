61b70f7911559996c60b9e960cccde380431f519
Prains <sleskov2013@yandex.ru>
2023-05-12 18:06:01 +0300

    try3

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import Advantage from "@/components/Mainpage/Advantage/Advantage";
-import Main from "@/components/Mainpage/Main/Main";
-import Valueables from "@/components/Mainpage/Valueables/Valueables";
+import Advantage from "@/components/MainPage/Advantage/Advantage";
+import Main from "@/components/MainPage/Main/Main";
+import Valueables from "@/components/MainPage/Valueables/Valueables";
 
 export default function Home() {
   return (
