a5741a266d2f2817dcc1404af16f50d4e2541265
Prains <sleskov2013@yandex.ru>
2023-05-15 23:49:12 +0300

    add login button state

--- components/Header/HeaderLoginButton/HeaderLoginButton.jsx
+++ components/Header/HeaderLoginButton/HeaderLoginButton.jsx
@@ -2,20 +2,23 @@
 import Link from "next/link";
 
 const HeaderLoginButton = ({ children, href, burger, close }) => {
+  const [loginVisibility, setLoginVisibility] = useState(false);
   return (
-    <Link
-      href={href}
-      className={`bg-white-orange py-[11px] w-[120px] lg:flex items-center justify-center ${
-        burger ? "flex" : "hidden"
-      } hover:drop-shadow-roundedButton active:bg-darker-orange`}
-      onClick={() => {
-        if (close) {
-          close();
-        }
-      }}
-    >
-      {children}
-    </Link>
+    <>
+      <Link
+        href={href}
+        className={`bg-white-orange py-[11px] w-[120px] lg:flex items-center justify-center ${
+          burger ? "flex" : "hidden"
+        } hover:drop-shadow-roundedButton active:bg-darker-orange`}
+        onClick={() => {
+          if (close) {
+            close();
+          }
+        }}
+      >
+        {children}
+      </Link>
+    </>
   );
 };
 
