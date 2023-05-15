277a9f4bbcab0c57ae1109ba07ba3feb59f24cee
Prains <sleskov2013@yandex.ru>
2023-05-15 13:29:15 +0300

    add error popup

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import Advantage from "@/components/MainPage/Advantage/Advantage";
 import Main from "@/components/MainPage/Main/Main";
 import Valueables from "@/components/MainPage/Valueables/Valueables";
-import LoginPopup from "@/components/Popups/LoginPopup/LoginPopup";
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col gap-[38px] lg:gap-[90px]">
-      <LoginPopup />
       <Main />
       <Valueables />
       <Advantage />
