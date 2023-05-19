1198d85cdb0a1de33bceca173f6f7f444600f771
Maya <pchelaamaya@gmail.com>
2023-05-19 13:49:53 +0300

    correct page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,13 @@
-// import Advantage from "@/components/MainPage/Advantage/Advantage";
-// import Main from "@/components/Main/Main";
-// import Valueables from "@/components/MainPage/Valueables/Valueables";
-import MyDocument from "@/components/PersonalPages/MyDocument/MyDocument";
-import PersonalCabinet from "@/components/PersonalPages/PersonalCabinet/PersonalCabinet";
+import Advantage from "@/components/MainPage/Advantage/Advantage";
+import Main from "@/components/Main/Main";
+import Valueables from "@/components/MainPage/Valueables/Valueables";
 
 export default function Home() {
   return (
-    // <main className="flex items-center justify-center flex-col gap-[38px] lg:gap-[90px]">
-    //   <Main />
-    //   <Valueables />
-    //   <Advantage />
-    // </main>
-    <main>
-      <MyDocument/>
+    <main className="flex items-center justify-center flex-col gap-[38px] lg:gap-[90px]">
+      <Main />
+      <Valueables />
+      <Advantage />
     </main>
   );
 }
