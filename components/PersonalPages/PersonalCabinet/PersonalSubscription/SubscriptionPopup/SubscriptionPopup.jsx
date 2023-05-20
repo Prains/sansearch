19cd70ca24860c490dc0a6de05613f71c8ef7d43
Maya <pchelaamaya@gmail.com>
2023-05-20 23:10:08 +0300

    added parts subscriptionpopup

--- components/PersonalPages/PersonalCabinet/PersonalSubscription/SubscriptionPopup/SubscriptionPopup.jsx
+++ components/PersonalPages/PersonalCabinet/PersonalSubscription/SubscriptionPopup/SubscriptionPopup.jsx
@@ -1,6 +1,10 @@
-const SubscriptionPopup = () => {
+import Overlay from "@/components/ui/Overlay";
+
+const SubscriptionPopup = (isSubscriptionPopup) => {
     return (
-        <></>
+        <>
+        <Overlay isSubscriptionPopup={isSubscriptionPopup}/>
+        </>
     )
 }
 export default SubscriptionPopup;
