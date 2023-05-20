8006a9159d56d5864b94d3c7aafb7ddec8d47af4
Maya <pchelaamaya@gmail.com>
2023-05-20 15:01:17 +0300

    fixed formprofile

--- components/PersonalPages/PersonalCabinet/PersonalProfile/FormProfile/FormProfile.jsx
+++ components/PersonalPages/PersonalCabinet/PersonalProfile/FormProfile/FormProfile.jsx
@@ -11,11 +11,9 @@ import { useState } from "react";
 const FormProfile = () => {
 
     const [name, nameChange] = useInput("");
-    const [surname, surnameChange] = useInput("");
     const [email, emailChange] = useInput("");
     const [password, passwordChange] = useInput("");
     const [newPassword, newPasswordChange] = useInput("");
-    const [company, companyChange] = useInput("");
 
     const [isAddButtons, isSetAddButtons] = useState(false);
 
@@ -47,12 +45,32 @@ const FormProfile = () => {
             },
         },
         {
-            label: "Фамилия:",
-            htmlType: "text",
-            placeholder: "Введите вашу фамилию",
-            defaultValue: users[0].surname,
+            label: "Почта:",
+            htmlType: "email",
+            placeholder: "Введите ваш email",
+            defaultValue: users[0].email,
             onChange: (e) => {
-                surnameChange(e);
+                emailChange(e);
+            },
+        },
+        {
+            label: "Пароль:",
+            htmlType: "password",
+            placeholder: "Введите ваш пароль",
+            defaultValue: users[0].password,
+            onChange: (e) => {
+                passwordChange(e);
+            },
+        },
+    ]
+    const inputCorrect = [
+        {
+            label: "Имя:",
+            htmlType: "name",
+            placeholder: "Введите ваше имя",
+            defaultValue: users[0].name,
+            onChange: (e) => {
+                nameChange(e);
             },
         },
         {
@@ -82,16 +100,6 @@ const FormProfile = () => {
                 newPasswordChange(e);
             },
         },
-        {
-            label: "Компания:",
-            htmlType: "text",
-            placeholder: "Введите вашу компанию",
-            defaultValue: users[0].company,
-            onChange: (e) => {
-                companyChange(e);
-            },
-        },
-
     ]
 
     return (
@@ -103,15 +111,28 @@ const FormProfile = () => {
                 alt="Фотография пользователя"
             />
             <div className="w-[270px] max-h-[600px] mx-auto text-left lg:w-[416px]">
-                {inputInfo.map((input) => {
-                    return (
-                    <LabelInput
-                        {...input}
-                        key={input.label}
-                        isAddButtons={isAddButtons}
-                    />
-                    );
-                })}
+                {/* {isAddButtons ? (
+                    {inputCorrect.map((input) => {
+                        return (
+                        <LabelInput
+                            {...input}
+                            key={input.label}
+                            isAddButtons={isAddButtons}
+                        />
+                        )
+                    })
+                    }
+                ) : (
+                    {inputInfo.map((input) => {
+                        return (
+                        <LabelInput
+                            {...input}
+                            key={input.label}
+                            isAddButtons={isAddButtons}
+                        />
+                        );
+                    })}                    
+                )} */}
                 <label className="text-[22px] mr-1 lg:inline-block lg:w-[178px]">Подписка:</label>
                 {users[0].subscription ? (
                     <span className="text-[20px] text-[#69D443]">Есть</span>
