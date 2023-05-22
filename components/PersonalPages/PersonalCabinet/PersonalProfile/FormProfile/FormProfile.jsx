7a7fcc37dadadcbcbcc263355fe9b38e0579374e
Prains <sleskov2013@yandex.ru>
2023-05-22 23:18:20 +0300

    make popup close when click on exit cross

--- components/PersonalPages/PersonalCabinet/PersonalProfile/FormProfile/FormProfile.jsx
+++ components/PersonalPages/PersonalCabinet/PersonalProfile/FormProfile/FormProfile.jsx
@@ -11,146 +11,151 @@ import Loader from "@/components/ui/Loader";
 import ProfileDeletePopup from "../ProfileDeletePopup/ProfileDeletePopup";
 
 const FormProfile = () => {
-    const { user } = useSelector((state) => state.user);
-    const [name, nameChange] = useInput(user?.username);
-    const [email, emailChange] = useInput(user?.email);
-    const [password, passwordChange] = useInput(user?.password);
-    const [isAddButtons, isSetAddButtons] = useState(false);
-    const [isOpenPopupDeleteProfile, isSetOpenPopupDeleteProfile] = useState(false);
+  const { user } = useSelector((state) => state.user);
+  const [name, nameChange] = useInput(user?.username);
+  const [email, emailChange] = useInput(user?.email);
+  const [password, passwordChange] = useInput(user?.password);
+  const [isAddButtons, isSetAddButtons] = useState(false);
+  const [isOpenPopupDeleteProfile, isSetOpenPopupDeleteProfile] =
+    useState(false);
 
-    if (!user) {
-        return (
-            <Loader/>
-        )
-    }
-    const handleClickAddButtons = () => {
-        isSetAddButtons((isAddButtons) => (!isAddButtons))
-    }
-    
-    const handleClickOpenDeleteProfile = () => {
-        isSetOpenPopupDeleteProfile((isOpenPopupDeleteProfile) => (!isOpenPopupDeleteProfile))
-    }
+  if (!user) {
+    return <Loader />;
+  }
+  const handleClickAddButtons = () => {
+    isSetAddButtons((isAddButtons) => !isAddButtons);
+  };
 
-    const inputInfo = [
-        {
-            label: "Имя:",
-            htmlType: "name",
-            placeholder: "Введите ваше имя",
-            defaultValue: name,
-            onChange: (e) => {
-                nameChange(e);
-            },
-        },
-        {
-            label: "Почта:",
-            htmlType: "email",
-            placeholder: "Введите ваш email",
-            defaultValue: email,
-            onChange: (e) => {
-                emailChange(e);
-            },
-        },
-        {
-            label: "Пароль:",
-            htmlType: "password",
-            placeholder: "Введите ваш пароль",
-            deafultValue: password,
-            onChange: (e) => {
-                passwordChange(e);
-            },
-        },
-    ]
-    const inputCorrect = [
-        {
-            label: "Имя:",
-            htmlType: "name",
-            placeholder: "Введите ваше имя",
-            defaultValue: name,
-            onChange: (e) => {
-                nameChange(e);
-            },
-        },
-        {
-            label: "Почта:",
-            htmlType: "email",
-            placeholder: "Введите ваш email",
-            defaultValue: email,
-            onChange: (e) => {
-                emailChange(e);
-            },
-        },
-        {
-            label: "Новый пароль:",
-            htmlType: "text",
-            placeholder: "Введите новый пароль",
-            defaultValue: password,
-            onChange: (e) => {
-                passwordChange(e);
-            },
-        },
-    ]
+  const handleClickOpenDeleteProfile = () => {
+    isSetOpenPopupDeleteProfile(
+      (isOpenPopupDeleteProfile) => !isOpenPopupDeleteProfile
+    );
+  };
 
-    return (
-        <form className="m-h-[800px]">
-            <Image 
-                className="w-[100px] h-[100px] mx-auto mb-[20px]" 
-                src={avatar} 
-                alt="Фотография пользователя"
-            />
-            <div className="w-[270px] max-h-[600px] mx-auto text-left lg:w-[416px]">
-            {isAddButtons ? inputCorrect.map((input) => {
-                            return (
-                            <LabelInput
-                                {...input}
-                                key={input.label}
-                                isAddButtons={isAddButtons}
-                            />
-                        )
-                    }) : inputInfo.map((input) => {
-                            return (
-                            <LabelInput
-                                {...input}
-                                key={input.label}
-                                isAddButtons={isAddButtons}
-                            />
-                        );
-                    })}                    
+  const inputInfo = [
+    {
+      label: "Имя:",
+      htmlType: "name",
+      placeholder: "Введите ваше имя",
+      defaultValue: name,
+      onChange: (e) => {
+        nameChange(e);
+      },
+    },
+    {
+      label: "Почта:",
+      htmlType: "email",
+      placeholder: "Введите ваш email",
+      defaultValue: email,
+      onChange: (e) => {
+        emailChange(e);
+      },
+    },
+    {
+      label: "Пароль:",
+      htmlType: "password",
+      placeholder: "Введите ваш пароль",
+      deafultValue: password,
+      onChange: (e) => {
+        passwordChange(e);
+      },
+    },
+  ];
+  const inputCorrect = [
+    {
+      label: "Имя:",
+      htmlType: "name",
+      placeholder: "Введите ваше имя",
+      defaultValue: name,
+      onChange: (e) => {
+        nameChange(e);
+      },
+    },
+    {
+      label: "Почта:",
+      htmlType: "email",
+      placeholder: "Введите ваш email",
+      defaultValue: email,
+      onChange: (e) => {
+        emailChange(e);
+      },
+    },
+    {
+      label: "Новый пароль:",
+      htmlType: "text",
+      placeholder: "Введите новый пароль",
+      defaultValue: password,
+      onChange: (e) => {
+        passwordChange(e);
+      },
+    },
+  ];
 
-                <label className="text-[22px] mr-1 lg:inline-block lg:w-[178px]">Подписка:</label>
-                {user?.subscribed ? (
-                    <span className="text-[20px] text-[#69D443]">Есть</span>
-                    ) : (
-                    <span className="text-[20px] text-white-black">Нет</span>
-                )}
-            </div>
-            <div className="mt-[30px]">
-                {isAddButtons ? ( 
-                   <div>
-                        <Buttons 
-                            type="orange" 
-                            onClick={handleClickAddButtons}
-                        >
-                            Сохранить изменения
-                        </Buttons>
-                        <Buttons 
-                            type="grey"
-                            className="w-[280px] lg:w-[416px]"
-                            onClick={handleClickOpenDeleteProfile}
-                        >
-                            Удалить аккаунт
-                        </Buttons>
-                    </div>
-               ) : (
-                <Button 
-                    type="secondary" 
-                    className="mx-auto text-full-white h-[45px] lg:w-[416px]" 
-                    onClick={handleClickAddButtons}
-                >
-                    Изменить данные
-                </Button>
-                )}
-            </div>
-            {isOpenPopupDeleteProfile && <ProfileDeletePopup/>}
-        </form>
-    )}
+  return (
+    <form className="m-h-[800px]">
+      <Image
+        className="w-[100px] h-[100px] mx-auto mb-[20px]"
+        src={avatar}
+        alt="Фотография пользователя"
+      />
+      <div className="w-[270px] max-h-[600px] mx-auto text-left lg:w-[416px]">
+        {isAddButtons
+          ? inputCorrect.map((input) => {
+              return (
+                <LabelInput
+                  {...input}
+                  key={input.label}
+                  isAddButtons={isAddButtons}
+                />
+              );
+            })
+          : inputInfo.map((input) => {
+              return (
+                <LabelInput
+                  {...input}
+                  key={input.label}
+                  isAddButtons={isAddButtons}
+                />
+              );
+            })}
+
+        <label className="text-[22px] mr-1 lg:inline-block lg:w-[178px]">
+          Подписка:
+        </label>
+        {user?.subscribed ? (
+          <span className="text-[20px] text-[#69D443]">Есть</span>
+        ) : (
+          <span className="text-[20px] text-white-black">Нет</span>
+        )}
+      </div>
+      <div className="mt-[30px]">
+        {isAddButtons ? (
+          <div>
+            <Buttons type="orange" onClick={handleClickAddButtons}>
+              Сохранить изменения
+            </Buttons>
+            <Buttons
+              type="grey"
+              className="w-[280px] lg:w-[416px]"
+              onClick={handleClickOpenDeleteProfile}
+            >
+              Удалить аккаунт
+            </Buttons>
+          </div>
+        ) : (
+          <Button
+            type="secondary"
+            className="mx-auto text-full-white h-[45px] lg:w-[416px]"
+            onClick={handleClickAddButtons}
+          >
+            Изменить данные
+          </Button>
+        )}
+      </div>
+      {isOpenPopupDeleteProfile && (
+        <ProfileDeletePopup close={handleClickOpenDeleteProfile} />
+      )}
+    </form>
+  );
+};
 export default FormProfile;
--- components/PersonalPages/PersonalCabinet/PersonalSubscription/PersonalSubscription.jsx
+++ components/PersonalPages/PersonalCabinet/PersonalSubscription/PersonalSubscription.jsx
@@ -5,27 +5,29 @@ import { useState } from "react";
 import SubscriptionPopup from "./SubscriptionPopup/SubscriptionPopup";
 
 const PersonalSubscription = () => {
-    const [isSubscriptionPopup, isSetSubscriptionPopup] = useState(false);
+  const [isSubscriptionPopup, isSetSubscriptionPopup] = useState(false);
 
-    const handleClickOpenSubscriptionPupup = () => {
-        isSetSubscriptionPopup((isSubscriptionPopup) => (!isSubscriptionPopup));
-    }
+  const handleClickOpenSubscriptionPupup = () => {
+    isSetSubscriptionPopup((isSubscriptionPopup) => !isSubscriptionPopup);
+  };
 
-    return (
-        <article className="mt-[30px] mb-[15px] text-center mx-auto w-[280px] lg:mx-0 lg:text-left lg:w-[560px] lg:mt-0">
-            <PersonalSubtitle className="mb-[45px]">Подписка</PersonalSubtitle>
-            <p className="text-[20px] mb-[55px] lg:text-[24px] lg:mb-[30px]">
-                Оформите подписку для быстрого доступа к документам
-            </p>
-            <Button 
-                type="secondary" 
-                className="text-full-white h-[50px] lg:h-[60px]" 
-                onClick={handleClickOpenSubscriptionPupup}
-            >
-                Оформить
-            </Button>
-            {isSubscriptionPopup ? <SubscriptionPopup/> : ''}
-        </article>
-    )
-}
-export default PersonalSubscription
+  return (
+    <article className="mt-[30px] mb-[15px] text-center mx-auto w-[280px] lg:mx-0 lg:text-left lg:w-[560px] lg:mt-0">
+      <PersonalSubtitle className="mb-[45px]">Подписка</PersonalSubtitle>
+      <p className="text-[20px] mb-[55px] lg:text-[24px] lg:mb-[30px]">
+        Оформите подписку для быстрого доступа к документам
+      </p>
+      <Button
+        type="secondary"
+        className="text-full-white h-[50px] lg:h-[60px]"
+        onClick={handleClickOpenSubscriptionPupup}
+      >
+        Оформить
+      </Button>
+      {isSubscriptionPopup && (
+        <SubscriptionPopup close={handleClickOpenSubscriptionPupup} />
+      )}
+    </article>
+  );
+};
+export default PersonalSubscription;
