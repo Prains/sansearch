c6d52db0409c3ad4feb5cdabc4406eef9c0096c2
Prains <sleskov2013@yandex.ru>
2023-05-23 22:36:32 +0300

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import Providers from "@/components/Providers";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "SanSearch",
@@ -12,6 +13,17 @@ export default function RootLayout({ children, auth }) {
   return (
     <html lang="ru">
       <body className="font-text" suppressHydrationWarning={true}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4TFQC8LTVE"
+          strategy="afterInteractive"
+        />
+        <Script strategy="afterInteractive" id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4TFQC8LTVE');`}
+        </Script>
         <Providers>
           <Header />
           {children}
