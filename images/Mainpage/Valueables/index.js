1689d2369aedd3378eb59a1cd143419f0bed0376
Prains <sleskov2013@yandex.ru>
2023-05-23 15:53:47 +0300

    edit valuables images

--- images/Mainpage/Valueables/index.js
+++ images/Mainpage/Valueables/index.js
@@ -1,6 +1,6 @@
 import comfort from "./access-card-form-svgrepo-com 1.svg";
-import time from "./internet-speed-svgrepo-com 1.png";
-import access from "./platform-program-svgrepo-com.png";
-import financial from "./financial-report-svgrepo-com 1.png";
+import time from "./internet-speed-svgrepo-com 1.svg";
+import access from "./platform-program-svgrepo-com.svg";
+import financial from "./financial-report-svgrepo-com 1.svg";
 
 export { comfort, time, access, financial };
